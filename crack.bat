30d6ec3c32f7d82159da47fb6f7fcd03c910c42a
Горнак Александр Денисович <24509@dune.edu>
2024-10-01 14:14:47 +0200

    inital commit

---
+++ crack.bat
@@ -0,0 +1,5 @@
+c:
+copy /Y c:\Corel\PASMUTILITY.dll "c:\Program Files\Corel\PASMUtility\v1"
+"C:\Program Files\Corel\CorelDRAW Graphics Suite 2021\Programs64\CorelDRW.exe"
+copy /Y c:\corel\1\PASMUTILITY.dll "c:\Program Files\Corel\PASMUtility\v1"
+"C:\Program Files\Corel\CorelDRAW Graphics Suite 2021\Programs64\CorelDRW.exe"
